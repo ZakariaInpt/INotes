2eb163f1adf005c2cb5a4936484bf60f503f379a
toto TOTO <zakaria.elmrabet2@gmail.com>
2019-01-15 16:37:44 -0600

    Initial Commit

---
+++ INote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INote
+//
+//  Created by User on 15/01/2019.
+//  Copyright © 2019 Zakaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
