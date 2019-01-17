9976f0a352f9ede8ed7b48a5f73e92aa1573154a
toto TOTO <zakaria.elmrabet2@gmail.com>
2019-01-16 23:13:22 -0600

    Add the functionally of adding new items in the table and refresh the table's contente

--- INote/ToDoListViewController.swift
+++ INote/ToDoListViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
-    let cellArray=["IOS Business", "GYM", "Research Lab"]
+    
+    
+    var cellArray=["IOS Business", "GYM", "Research Lab"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     //MARK - Tabelview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cellArray.count
@@ -43,9 +44,31 @@ class ToDoListViewController: UITableViewController {
         
     }
     
+    //MARK - ADD a new items
+    
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        
+        var text = UITextField()
+        
+        let alert = UIAlertController (title: "Add New Item", message: "Would you like to add new item?", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.cellArray.append(text.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextfield) in
+            alertTextfield.placeholder="Create the new item"
+            text = alertTextfield
+            
+        }
+        
+        alert.addAction(action)
+        present(alert,animated: true, completion: nil)
+        
+        
+    }
     
     
     
-
 }
 
